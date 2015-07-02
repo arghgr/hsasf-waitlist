47405fea5f47ce9066fae41692af4fdf17960dda
Bianca <tenmillionpeople@gmail.com>
2015-07-01 20:17:34 -0700

    Added user show and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,15 +9,32 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = get_user
   end
 
   def update
+    @user = get_user
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'User settings updated.'
+    else
+      render 'edit', notice: 'There were one or more errors with your update. Please change them and try again.'
+    end
   end
 
   def show
+    @user = get_user
   end
 
   def destroy
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :birthdate, :phone)
+  end
+
+  def get_user
+    User.find(params[:id])
+  end
 end
